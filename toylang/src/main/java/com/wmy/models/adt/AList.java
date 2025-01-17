66813e21931af3e65a315626ff2a52a91203e487
Pascu-Victor <me@vpascu.ro>
2025-01-17 19:27:59 +0200

    make IList iterable

--- toylang/src/main/java/com/wmy/models/adt/AList.java
+++ toylang/src/main/java/com/wmy/models/adt/AList.java
@@ -1,6 +1,9 @@
 package com.wmy.models.adt;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -25,8 +28,8 @@ public class AList<T> implements IList<T> {
 
     public String toString() {
         return list.stream()
-               .map(Object::toString)
-               .collect(Collectors.joining("\n")) + "\n";
+                .map(Object::toString)
+                .collect(Collectors.joining("\n")) + "\n";
     }
 
     public int size() {
@@ -39,5 +42,25 @@ public class AList<T> implements IList<T> {
 
     public Stream<T> stream() {
         return list.stream();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return list.spliterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        list.forEach(action);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return list.equals(obj);
     }
 }
--- toylang/src/main/java/com/wmy/models/adt/IList.java
+++ toylang/src/main/java/com/wmy/models/adt/IList.java
@@ -2,12 +2,18 @@ package com.wmy.models.adt;
 
 import java.util.stream.Stream;
 
-public interface IList<T> {
+public interface IList<T> extends Iterable<T> {
     void add(T elem);
+
     void remove(T elem);
+
     boolean contains(T elem);
+
     int size();
+
     T get(int index);
+
     String toString();
+
     Stream<T> stream();
 }
