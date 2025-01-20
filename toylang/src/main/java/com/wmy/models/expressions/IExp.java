30b401be56131a84fef6d9568e3b53d053bfe2f1
Pascu-Victor <me@vpascu.ro>
2025-01-20 21:50:52 +0200

    IExp: make it clonable

--- toylang/src/main/java/com/wmy/models/expressions/IExp.java
+++ toylang/src/main/java/com/wmy/models/expressions/IExp.java
@@ -3,13 +3,16 @@ package com.wmy.models.expressions;
 import com.wmy.exceptions.ExecutionException;
 import com.wmy.exceptions.TypeException;
 import com.wmy.models.adt.CloneableString;
+import com.wmy.models.adt.ICloneable;
 import com.wmy.models.adt.IDict;
 import com.wmy.models.adt.IHeap;
 import com.wmy.models.types.IType;
 import com.wmy.models.values.IValue;
 
-public interface IExp {
+public interface IExp extends ICloneable {
     IValue eval(IDict<CloneableString, IValue> symTable, IHeap heap) throws ExecutionException;
+
     IType typecheck(IDict<CloneableString, IType> typeEnvironment) throws TypeException;
+
     IExp deepCopy();
 }
